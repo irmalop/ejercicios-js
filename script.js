8a2df4b6719debbbdd06ef471fb09edadac6d09f
Irma López González <ada_wit18@hackademy.mx>
2022-02-06 21:53:10 -0600

    Ejercicios 1 y 2

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const ejercicio1 = [
   3, 100, 85, 64, 46, 39, 40, 30, 20, 24, 25, 6, 10, 54, 82, 71, 67, 77, 17, 29,
   19, 88, 456, 13, 23, 24,
 ];
+let numbers = [];
+function esPrimo(numero){
+let raiz = Math.floor(Math.sqrt(numero)) + 1;
+for(let i = 2; i < raiz; i++){
+  if (numero % i == 0) {
+    return false;
+  }
+}
+numbers.push(numero);
+// console.log(`El ${numero} es primo`);
+return true;
+}
+ejercicio1.forEach((numero) =>{
+  esPrimo(numero);
+});
+console.log(numbers)
 
 const ejercicio2 = [
   {
@@ -35,3 +51,11 @@ const ejercicio2 = [
     esFamiliar: true,
   },
 ];
+const admitirPaso = (persona) => {
+    if (persona.edad >= 18 && persona.esFamiliar == true) {
+        console.log(`Puedes pasar ${persona.nombre}`)
+    } 
+}
+ejercicio2.forEach((persona) =>{
+    admitirPaso(persona);
+});
